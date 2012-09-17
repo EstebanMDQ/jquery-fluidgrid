6aea14c6d9874d23a0aba96c0e082ab46a8bc4c7
Esteban Soler <esteban@daemoniclabs.com>
2012-09-17 13:04:58 -0300

    removed the console logs so it doesn't have issues with IE

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ jquery-fluidgrid
 ================
 
 jQuery plugin to create fluid grid layouts
+
+Please checkout the example in http://mcniac.github.com/jquery-fluidgrid/
--- jquery.fluidgrid.js
+++ jquery.fluidgrid.js
@@ -18,7 +18,6 @@
 				gutter: 20
 			};
 		if(typeof(this.length)=="undefined" || this.length==0) {
-			console.log('no fluid grids in the page');
 			return false;
 		}
 		var options = $.extend({}, defaults, options),
@@ -34,7 +33,6 @@
 		var cc = Math.floor(this.width() / (iw + gu)), // columns count
 		fcols = []; // columns array
 
-		console.log("container width "+$(this).width()+ " calculated "+((iw + gu)*cc)+ " iw:"+iw);
 		// recalculate the item width and gutter so it completes the whole container
 		if( $(this).width() > (iw + gu)*cc ) {
 			iw += Math.floor(($(this).width() - (iw + gu)*cc)/cc*0.9);
@@ -45,7 +43,6 @@
 			fcols[cc] = 0;
 		}
 
-		console.log(iw, gu, cc, coffset, loffset, fcols);
 
 		// add each item to the shortest column
 		$(options.item_selector, this).each(function() {
@@ -54,7 +51,6 @@
 			lpos = idx*(iw+gu)+loffset+gu,
 			tpos = fcols[idx]+coffset;
 		
-//			console.log(idx,lpos,tpos);
 			$(this).css({position:'absolute',float:'left',top:tpos+'px',left:lpos+'px'})
 			fcols[idx] += $(this).height()+gu;
 //			$(this).detach();
